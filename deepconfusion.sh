f3d08c8de7d335defe466109bccc7a1806be238f
S1rN3tZ <40497633+mathis2001@users.noreply.github.com>
2023-12-19 11:52:56 +0100

    Add files via upload

--- deepconfusion.sh
+++ deepconfusion.sh
@@ -7,33 +7,58 @@ BLUE='\033[0;34m'
 RESET='\033[0m'
 
 if [ $# -eq 0 ]; then
-    echo -e "${RED}[!]${RESET} Usage: $0 <package.json>"
+    echo -e "${RED}[!]${RESET} Usage: $0 <package.json or requirements.txt>"
     exit 1
 fi
 
-if [ ! -f "$1" ]; then
-    echo -e "${RED}[!]${RESET} Error: File '$1' not found."
-    exit 1
-fi
+file_extension="${1##*.}"
 
-package_names=($(grep -oE '"[^"]+":' $1 | sed 's/"//g' | sed 's/:$//'))
-
-for package_name in "${package_names[@]}"; do
-    echo -e "Searching public npm package for ${GREEN}$package_name${RESET}..."
-    npm_search_response=$(npm search "$package_name" --json)
-    matching_package=$(echo "$npm_search_response" | jq -r '.[] | select(.name == "'"$package_name"'")')
-    
-    if [ -z "$matching_package" ]; then
-        echo -e "${RED}[!]${RESET} $package_name ${RED}NOT${RESET} in public registry !"
-        echo -e "${BLUE}[*]${RESET} Checking if package has been deprecated"
-	if curl -s https://www.npmjs.com/package/$package_name | grep -q 'has been deprecated'; then
-	    echo -e "${YELLOW}[-]${RESET} Here but ${YELLOW}deprecated${RESET}"
-	else
-	    echo $package_name >> confusion.txt
-	fi
-    else
-        echo -e "${GREEN}[+]${RESET} $package_name in public registry."
+if [ "$file_extension" == "json" ]; then
+    if [ ! -f "$1" ]; then
+        echo -e "${RED}[!]${RESET} Error: File '$1' not found."
+        exit 1
     fi
-    
-    echo "---------------------------------------------------"
-done
+
+    package_names=($(grep -oE '"[^"]+":' "$1" | sed 's/"//g' | sed 's/:$//'))
+
+    for package_name in "${package_names[@]}"; do
+        echo -e "Searching public npm package for ${GREEN}$package_name${RESET}..."
+        npm_search_response=$(npm search "$package_name" --json)
+        matching_package=$(echo "$npm_search_response" | jq -r '.[] | select(.name == "'"$package_name"'")')
+
+        if [ -z "$matching_package" ]; then
+            echo -e "${RED}[!]${RESET} $package_name ${RED}NOT${RESET} in public registry !"
+            echo -e "${BLUE}[*]${RESET} Checking if package has been deprecated"
+            if curl -s "https://www.npmjs.com/package/$package_name" | grep -q 'has been deprecated'; then
+                echo -e "${YELLOW}[-]${RESET} Here but ${YELLOW}deprecated${RESET}"
+            else
+                echo $package_name >> confusion.txt
+            fi
+        else
+            echo -e "${GREEN}[+]${RESET} $package_name in public registry."
+        fi
+
+        echo "---------------------------------------------------"
+    done
+
+elif [ "$file_extension" == "txt" ]; then
+    if [ ! -f "$1" ]; then
+        echo -e "${RED}[!]${RESET} Error: File '$1' not found."
+        exit 1
+    fi
+
+    cat "$1" \
+    | awk '{print $1;}' \
+    | tr -d '><~#$' \
+    | sort -u \
+    | cut -d '=' -f 1 \
+    | awk '{print $1;}' \
+    | sed -r 's/[^[:space:]]*[0-9][^[:space:]]* ?//g' \
+    | sort -u \
+    | xargs -n1 -I{} echo "https://pypi.org/project/{}/" \
+    | httpx -status-code -silent
+
+else
+    echo -e "${RED}[!]${RESET} Unsupported file extension. Only ${GREEN}JSON${RESET} and ${GREEN}TXT${RESET} files are supported."
+    exit 1
+fi
