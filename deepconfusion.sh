b15298a702418eb942d929ecc582deeb454ca67c
S1rN3tZ <40497633+mathis2001@users.noreply.github.com>
2023-12-19 10:34:58 +0100

    Add files via upload

--- deepconfusion.sh
+++ deepconfusion.sh
@@ -3,6 +3,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
 RESET='\033[0m'
 
 if [ $# -eq 0 ]; then
@@ -24,7 +25,8 @@ for package_name in "${package_names[@]}"; do
     
     if [ -z "$matching_package" ]; then
         echo -e "${RED}[!]${RESET} $package_name ${RED}NOT${RESET} in public registry !"
-	if curl -s https://www.npmjs.com/package/$package_name | grep -q 'no longer maintained'; then
+        echo -e "${BLUE}[*]${RESET} Checking if package has been deprecated"
+	if curl -s https://www.npmjs.com/package/$package_name | grep -q 'has been deprecated'; then
 	    echo -e "${YELLOW}[-]${RESET} Here but ${YELLOW}deprecated${RESET}"
 	else
 	    echo $package_name >> confusion.txt
